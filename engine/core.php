b6f936803a8e805c58bec733d9d73d08eb1c4abe
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-15 23:30:49 +0200

    Setting, checking, protecting the session name

--- engine/core.php
+++ engine/core.php
@@ -1,1 +1,8 @@
-core.php
+<?php
+
+# Підключення ключових конфігураційних файлів системи
+require_once(ENGINE_DIR . '/system/global/config.php');
+require_once(ENGINE_DIR . '/system/global/connect.php');
+require_once(ENGINE_DIR . '/system/global/users.php');
+
+# Функції розміщення шапки та підвалу сторінок
