f67375a4202d6270dba26dd6b85f72a3363c8e1f
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-16 01:24:26 +0200

    Filter and set SERVER_NAME constant for server name

--- engine/system/global/include/constants.php
+++ engine/system/global/include/constants.php
@@ -6,6 +6,7 @@ $serverData = [
     'TM' => time(),                                                                                                                                                         // Поточний системний час
     'PHP_SELF' => _filter($_SERVER['PHP_SELF']),                                                                                                                            // Ім'я файлу, до якого виконується звернення
     'HTTP_HOST' => _filter($_SERVER['HTTP_HOST']),                                                                                                                          // Домен сайту
+    'SERVER_NAME' => _filter($_SERVER['SERVER_NAME']),                                                                                                                      // Ім'я сервера
 ];
 
 # Визначаємо константи з масиву
