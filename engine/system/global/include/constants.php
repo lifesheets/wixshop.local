457caa28bdd03574c007c82414fddf3cf7780f1f
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-16 01:24:41 +0200

    Determine and set SCHEME constant (http/https)

--- engine/system/global/include/constants.php
+++ engine/system/global/include/constants.php
@@ -7,6 +7,7 @@ $serverData = [
     'PHP_SELF' => _filter($_SERVER['PHP_SELF']),                                                                                                                            // Ім'я файлу, до якого виконується звернення
     'HTTP_HOST' => _filter($_SERVER['HTTP_HOST']),                                                                                                                          // Домен сайту
     'SERVER_NAME' => _filter($_SERVER['SERVER_NAME']),                                                                                                                      // Ім'я сервера
+    'SCHEME' => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http',                                                                                  // Схема (http/https)
 ];
 
 # Визначаємо константи з масиву
