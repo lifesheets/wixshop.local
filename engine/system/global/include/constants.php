1fc72dc61ed392a12b1cbbd48e86f1aa7fa9bb73
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-16 01:35:35 +0200

    Define HTTP_REFERER constant for incoming referer URL

--- engine/system/global/include/constants.php
+++ engine/system/global/include/constants.php
@@ -2,17 +2,28 @@
 
 # Масив для збереження значень
 $serverData = [
-    'ROOT' => rtrim($_SERVER['DOCUMENT_ROOT'], '/'),                                                                                                              // Шлях від кореневої директорії
-    'TM' => time(),                                                                                                                                                         // Поточний системний час
-    'PHP_SELF' => _filter($_SERVER['PHP_SELF']),                                                                                                                            // Ім'я файлу, до якого виконується звернення
-    'HTTP_HOST' => _filter($_SERVER['HTTP_HOST']),                                                                                                                          // Домен сайту
-    'SERVER_NAME' => _filter($_SERVER['SERVER_NAME']),                                                                                                                      // Ім'я сервера
-    'SCHEME' => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http',                                                                                  // Схема (http/https)
-    'FULL_URL' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . _filter($_SERVER['HTTP_HOST']) . _filter($_SERVER['PHP_SELF']),     // Повний URL, включаючи схему, домен та шлях
-    'REFERER' => _filter($_SERVER['HTTP_REFERER'] ?? '')                                                                                                               // Реферер
+    # Шлях від кореневої директорії
+    'ROOT' => rtrim($_SERVER['DOCUMENT_ROOT'], '/'),
+    # Поточний системний час
+    'TM' => time(),
+    # Ім'я файлу, до якого виконується звернення
+    'PHP_SELF' => _filter($_SERVER['PHP_SELF']),
+    # Домен сайту
+    'HTTP_HOST' => _filter($_SERVER['HTTP_HOST']),
+    # Ім'я сервера
+    'SERVER_NAME' => _filter($_SERVER['SERVER_NAME']),
+    # Схема (http/https)
+    'SCHEME' => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http',
+    # Повний URL, включаючи схему, домен та шлях
+    'FULL_URL' => (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ? 'https' : 'http') . '://' . _filter($_SERVER['HTTP_HOST']) . _filter($_SERVER['PHP_SELF']),
+    # Реферер
+    'REFERER' => _filter($_SERVER['HTTP_REFERER'] ?? 'none'),
+    # Откуда пришли
+    'HTTP_REFERER' => isset($_SERVER['HTTP_REFERER']) ? _filter($_SERVER['HTTP_REFERER']) : 'none',
 ];
 
 # Визначаємо константи з масиву
 foreach ($serverData as $key => $value) {
     define($key, $value);
 }
+
