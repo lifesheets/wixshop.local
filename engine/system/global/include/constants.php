a0a73728e5cd4905ec34ec206aaa491ebfef3412
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-16 01:24:19 +0200

    Filter and set HTTP_HOST constant for website domain

--- engine/system/global/include/constants.php
+++ engine/system/global/include/constants.php
@@ -5,6 +5,7 @@ $serverData = [
     'ROOT' => rtrim($_SERVER['DOCUMENT_ROOT'], '/'),                                                                                                              // Шлях від кореневої директорії
     'TM' => time(),                                                                                                                                                         // Поточний системний час
     'PHP_SELF' => _filter($_SERVER['PHP_SELF']),                                                                                                                            // Ім'я файлу, до якого виконується звернення
+    'HTTP_HOST' => _filter($_SERVER['HTTP_HOST']),                                                                                                                          // Домен сайту
 ];
 
 # Визначаємо константи з масиву
