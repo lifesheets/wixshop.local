2b1f8e0006e2e9e479b59bf1c0cb2bbbfe7bcd03
ua.lifesheets <ua.lifesheets@gmail.com>
2025-02-16 02:08:27 +0200

    A function for working with POST parameters

--- engine/system/global/include/functions.php
+++ engine/system/global/include/functions.php
@@ -6,3 +6,9 @@ function get($data, $d = 0) {
     return isset($_GET[$data]) ? ($d == 0 ? remove_script($_GET[$data]) : $_GET[$data]) : null;
 }
 
+# Функція для роботи з параметрами POST
+function post($data, $d = 0) {
+    # Перевіряємо, чи існує параметр у POST-запиті
+    return isset($_POST[$data]) ? ($d == 0 ? remove_script($_POST[$data]) : $_POST[$data]) : null;
+}
+
